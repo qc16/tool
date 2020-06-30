0ad24fa677d5f5b5a6e096b835d6ad381bcd66b2
qc90 <qc90@foxmail.com>
2020-06-30 18:55:52 +0800

    update

--- sync/lockAndCreate.js
+++ sync/lockAndCreate.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 const sc = require('socket.io-client')
 const { blake2AsHex, decodeAddress } = require('@polkadot/util-crypto')
 const {
-  hexToString, isHex, u8aToHex, hexStripPrefix
+    hexToString, isHex, u8aToHex, hexStripPrefix
 } = require('@polkadot/util')
 const log4js = require('log4js')
 const bs58 = require('bs58')
@@ -59,19 +59,19 @@ async function sleep(second) {
 }
 
 function getDidHash(hex) {
-  const did = hexToDid(hex)
-  const didHash = didToHex(did)
-  return didHash
+    const did = hexToDid(hex)
+    const didHash = didToHex(did)
+    return didHash
 }
 
 function hexToDid(hex) {
-  const bytes = Buffer.from(hexStripPrefix(hex), 'hex')
-  const address = bs58.encode(bytes)
-  if (address) {
-    return `did:pra:${address}`
-  }
+    const bytes = Buffer.from(hexStripPrefix(hex), 'hex')
+    const address = bs58.encode(bytes)
+    if (address) {
+        return `did:pra:${address}`
+    }
 
-  return ''
+    return ''
 }
 
 function didToHex(did) {
@@ -79,40 +79,59 @@ function didToHex(did) {
     const bytes = bs58.decode(did.substring(8))
     return blake2AsHex(bytes, 256)
 }
-
-async function createAndLock() {
-  const data = await getSqlData(`select * from data_event where module_id ='did' and event_id = 'Created' order by block_id,extrinsic_idx`)
-  let i = 0
-  for (let item of data) {
-      const [ did ] = JSON.parse(item.attributes)
-      const didHash = getDidHash(did.value)
-      const element = await getSqlData(`select * from chain_v1_data where did_hash = '${didHash}'`)
-      if (!element[0]) continue
-      const { superior, address, wxid, reserved } = element[0]
-      const pubkey = u8aToHex(decodeAddress(address))
-      const params = {
-        superior,
-        address,
-        pubkey,
-        didType: '1',
-        socialAccount: wxid
-      }
-      logger.info(i, params, 'create params')
-      socket.emit('create_by_old', params)
-      if (reserved > 0) {
-        console.log(didHash, reserved, 'did hash')
-        await sleep(1)
-        const lockParams = {
-          address: '5CrRpNbQBTiBmTjpUgJ6mH9YRmopVweLsjffVz7muskYEo2r',
-          method: 'forceLock',
-          params: [didHash, reserved]
+async function create() {
+    const data = await getSqlData(`select address,superior,social_account_hash from data_did where address not in (select address from data_did_new) and social_account_hash is not null`)
+    let i = 0
+    for (let element of data) {
+        const { superior, address, social_account_hash: socialAccount } = element
+        const pubkey = u8aToHex(decodeAddress(address))
+        const params = {
+            superior,
+            address,
+            pubkey,
+            didType: '1',
+            socialAccount,
+            isHash: true
         }
-        logger.info(lockParams, 'lock params')
-        socket.emit('sign', lockParams)
-      }
-      await sleep(0.2)
-      i++
-  }
+        logger.info(i, params, 'create params')
+        socket.emit('create_by_old', params)
+        await sleep(0.2)
+        i++
+    }
+}
+async function createAndLock() {
+    const data = await getSqlData(`select * from data_event where module_id ='did' and event_id = 'Created' order by block_id,extrinsic_idx`)
+    let i = 0
+    for (let item of data) {
+        const [did] = JSON.parse(item.attributes)
+        const didHash = getDidHash(did.value)
+        const element = await getSqlData(`select * from chain_v1_data where did_hash = '${didHash}'`)
+        if (!element[0]) continue
+        const { superior, address, wxid, reserved } = element[0]
+        const pubkey = u8aToHex(decodeAddress(address))
+        const params = {
+            superior,
+            address,
+            pubkey,
+            didType: '1',
+            socialAccount: wxid
+        }
+        logger.info(i, params, 'create params')
+        socket.emit('create_by_old', params)
+        if (reserved > 0) {
+            console.log(didHash, reserved, 'did hash')
+            const lockParams = {
+                address: '5CrRpNbQBTiBmTjpUgJ6mH9YRmopVweLsjffVz7muskYEo2r',
+                method: 'forceLock',
+                params: [didHash, reserved]
+            }
+            logger.info(lockParams, 'lock params')
+            socket.emit('sign', lockParams)
+            await sleep(1)
+        }
+        await sleep(0.2)
+        i++
+    }
 }
 
 socket.on('succeed', message => {
@@ -129,7 +148,8 @@ socket.on('tx_failed', msg => {
 
 socket.on('connect', async () => {
     try {
-        createAndLock()
+        // createAndLock()
+        create()
     } catch (error) {
         console.log(error)
     }
